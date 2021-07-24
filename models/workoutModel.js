66af88b91f6a1974537ec8fed4051b8ea9b82124
Richo Ha <richard.dqh@gmail.com>
2021-07-24 17:35:42 +0800

    Added schema

---
+++ models/workoutModel.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const workout = new Schema({
+  day: {
+    type: Date,
+    default: Date.now(),
+  },
+  exercises: [{
+    type: {
+      type: String,
+      trim: true,
+      required: "Type of exercise is required"
+    },
+    name: {
+      type: String,
+      trim: true,
+      required: "Name of the exercise is required"
+    },
+    duration: {
+      type: Number,
+      required: "Duration of the excercise is required"
+    },
+    weight: {
+      type: Number,
+    },
+    reps: {
+      type: Number,
+    },
+    sets: {
+      type: Number,
+    },
+    distance: {
+      type: Number,
+    }
+  }]
+});
+const Workout = mongoose.model("Workout", workout);
+module.exports = Workout;
