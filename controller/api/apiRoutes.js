e9e944e900f7d58516e003c070643eaa061a0260
Richo Ha <richard.dqh@gmail.com>
2021-07-25 17:13:51 +0800

    added routes

---
+++ controller/api/apiRoutes.js
@@ -0,0 +1,71 @@
+const router = require('express').Router();
+
+// bring in Workout model
+const Workout = require('../../models/Workout');
+
+//GET Routes: /api/workouts
+router.get('/workouts', async (req, res) => {
+    try {
+        const workouts = await Workout.find({});
+        res.send(workouts);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json('Workouts could not be found.');
+    }
+});
+
+//POST Routes: /api/workouts
+router.post('/workouts', async (req, res) => {
+    try {
+        const addedWorkout = await Workout.create(req.body);
+        res.json(addedWorkout);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json('Workout could not be created.');
+    }
+});
+
+//PUT/UPDATE Routes: /api/workouts/id
+router.put('/workouts/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+        const updatedWorkout = await Workout.findByIdAndUpdate(
+            id,
+            {
+                $push: { exercises: req.body },
+            },
+            {
+                new: true,
+            }
+        );
+        res.status(200).json(updatedWorkout);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json('Workout could not be updated');
+    }
+});
+
+//GET Routes: /api/workouts/range
+router.get("/api/workouts/range", async (req, res) => {
+    try {
+        const workouts = await db.Workout.aggregate([
+            {
+                $addFields: {
+                    totalDuration: {
+                        $sum: '$exercises.duration'
+                    },
+                    totalWeight: {
+                        $sum: '$exercises.weight'
+                    }
+                },
+            },
+        ])
+            .limit(7);
+        res.json(workouts);
+    } catch (err) {
+        console.log(err);
+        response.status(500).send(err.message);
+    }
+});
+
+module.exports = router;
---
+++ controller/index.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const path = require('path');
+const apiRoutes = require('./api/apiRoutes');
+
+router.use('/api', apiRoutes);
+// exercise page routing
+router.get('/exercise', async (req, res) => {
+    try {
+        res.sendFile(path.join(__dirname, '../public/exercise.html'));
+    } catch (error) {
+        console.error(error);
+        res.status(500);
+    }
+});
+// stats page routing
+router.get('/stats', async (req, res) => {
+    try {
+        res.sendFile(path.join(__dirname, '../public/stats.html'));
+    } catch (error) {
+        console.error(error);
+        res.status(500);
+    }
+});
+module.exports = router;
